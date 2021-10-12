b812e38db8cd9ba0796a6e25f25d38944e8a6624
a-koro <korove@hotmail.com>
2021-10-12 17:42:36 +0300

    first commit

---
+++ src/main/java/com/korovesys/virtualZoo/VirtualZooApplication.java
@@ -0,0 +1,13 @@
+package com.korovesys.virtualZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korovesys/virtualZoo/VirtualZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korovesys.virtualZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
