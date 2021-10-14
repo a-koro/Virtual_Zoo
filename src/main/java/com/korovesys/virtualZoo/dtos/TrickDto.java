86bbc7e59ed41364a39fde6f588ed730e47b3525
a-koro <korove@hotmail.com>
2021-10-14 12:49:54 +0300

    New dto to build trick response

---
+++ src/main/java/com/korovesys/virtualZoo/dtos/TrickDto.java
@@ -0,0 +1,13 @@
+package com.korovesys.virtualZoo.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class TrickDto implements Serializable {
+
+    private String trick;
+}
