de1678141493a1767c1bb5f6e3b03a59238ce6fc
a-koro <korove@hotmail.com>
2021-10-14 12:50:30 +0300

    Added endpoints to teach and do trick

--- src/main/java/com/korovesys/virtualZoo/controllers/ZooCatalogController.java
+++ src/main/java/com/korovesys/virtualZoo/controllers/ZooCatalogController.java
@@ -3,37 +3,29 @@ package com.korovesys.virtualZoo.controllers;
 import com.korovesys.virtualZoo.entities.Animal;
 import com.korovesys.virtualZoo.projections.AnimalProjection;
 import com.korovesys.virtualZoo.services.AnimalServiceImpl;
+import com.korovesys.virtualZoo.services.SpeciesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
-public class ZooCatalog {
+public class ZooCatalogController {
 
     @Autowired
     AnimalServiceImpl animalService;
 
-    @GetMapping("/test")
-    public Animal getAnimals() {
+    @Autowired
+    SpeciesServiceImpl speciesService;
 
-        UUID id = UUID.fromString("e452695d-79fa-4e4d-8196-ffd4c7562e3b");
-
-        System.out.println(id);
-
-        Animal ddoo = animalService.findAnimalById(id);
-        return ddoo;
-    }
-
-    @GetMapping("/all")
+    @GetMapping("/allAnimalData")
     public List<Animal> getAllAnimals() {
 
         return animalService.findAllAnimals();
     }
 
-    @GetMapping("/allP")
+    @GetMapping("/all")
     public List<AnimalProjection> getAllAnimalsToProjections() {
         return animalService.getAllAnimalsToProjection();
     }
