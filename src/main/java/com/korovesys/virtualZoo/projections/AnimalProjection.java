a34dde3281a4bd062725d6c8c1296f985b9572b3
a-koro <korove@hotmail.com>
2021-10-14 12:51:53 +0300

    Updated projection

--- src/main/java/com/korovesys/virtualZoo/projections/AnimalProjection.java
+++ src/main/java/com/korovesys/virtualZoo/projections/AnimalProjection.java
@@ -2,11 +2,14 @@ package com.korovesys.virtualZoo.projections;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AnimalProjection {
     UUID getId();
     String getName();
     @Value("#{target.getSpecies().getName()}")
-    String getSpeciesName();
+    String getSpecies();
+    @Value("#{target.getTricks().![name]}")
+    List<String> getTricks();
 }
